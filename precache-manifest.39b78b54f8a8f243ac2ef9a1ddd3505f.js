7eab354d6223ca8b9702698d5e4c5723abf98ac6
dinhnhan <nguyenduc1998k71@gmail.com>
2023-10-21 15:23:43 +0700

    Updates

---
+++ precache-manifest.39b78b54f8a8f243ac2ef9a1ddd3505f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ea64b16f9b30915c8fb4c04c084320",
+    "url": "/moviesworld-octo/index.html"
+  },
+  {
+    "revision": "cb1ca96c432e797741ae",
+    "url": "/moviesworld-octo/static/css/main.f29f85de.chunk.css"
+  },
+  {
+    "revision": "d74540e76371519f88a1",
+    "url": "/moviesworld-octo/static/js/2.fb586896.chunk.js"
+  },
+  {
+    "revision": "71dbad3ce33198e781d81f88e3bcefea",
+    "url": "/moviesworld-octo/static/js/2.fb586896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb1ca96c432e797741ae",
+    "url": "/moviesworld-octo/static/js/main.64e5c1b7.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/moviesworld-octo/static/js/main.64e5c1b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c50af24ced6765e91ea1",
+    "url": "/moviesworld-octo/static/js/runtime-main.a6d02344.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/moviesworld-octo/precache-manifest.39b78b54f8a8f243ac2ef9a1ddd3505f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moviesworld-octo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
